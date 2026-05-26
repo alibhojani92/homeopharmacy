87c1457801763979e33b40c4c48818f561175c95
Ali Bhojani <alibhojani92@gmail.com>
2026-05-27 02:29:57 +0530

    added logo whatsapp preview

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,30 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Shri Sainath Homoeopathic Pharmacy",
   description: "Buy Homeopathic Medicines Online",
+
+  openGraph: {
+    title: "Shri Sainath Homoeopathic Pharmacy",
+    description: "Buy Homeopathic Medicines Online",
+    url: "https://homeopharmacy.aliraza-19d.workers.dev",
+    siteName: "Shri Sainath Homoeopathic Pharmacy",
+    images: [
+      {
+        url: "/opengraph-image.webp",
+        width: 1200,
+        height: 630,
+        alt: "Shri Sainath Homoeopathic Pharmacy",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Shri Sainath Homoeopathic Pharmacy",
+    description: "Buy Homeopathic Medicines Online",
+    images: ["/opengraph-image.webp"],
+  },
 };
 
 export default function RootLayout({ children }) {
